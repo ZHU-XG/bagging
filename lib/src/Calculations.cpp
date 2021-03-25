e5626db2f83b35560ce1466bbebd685090fdb9e7
ZHU-XG <zhuxugui98@outlook.com>
2021-03-26 00:08:06 +0100

    numeric threshold finisded, start finding best split

--- lib/src/Calculations.cpp
+++ lib/src/Calculations.cpp
@@ -19,7 +19,7 @@ using std::string;
 using std::unordered_map;
 
 tuple<const Data, const Data> Calculations::partition(const Data& data, const Question& q) {
-  Data true_rows; // 只有一个属性是true，其他都是false
+  Data true_rows; 
   Data false_rows;
   
   for (const auto &row: data) {
@@ -33,32 +33,33 @@ tuple<const Data, const Data> Calculations::partition(const Data& data, const Qu
 }
 
 tuple<const double, const Question> Calculations::find_best_split(const Data& rows, const MetaData& meta) {
-  double best_gain = 0.0;  // keep track of the best information gain
-  auto best_question = Question();  //keep track of the feature / value that produced it
-  // TODO: find the best split among all features and feature values
-  // data二维，里面是v，外面是class
-  // classcounts 后返回class数和相应的个数
-  //const ClassCounter ctr = Calculations::classCounts(rows);  // rows是某个属性的二维表格
-  //const double gini_s = Calculations::gini(ctr, double(rows.size()));  // 某个属性的所有可能取值
+    double best_gain = 0.0;  // keep track of the best information gain
+    auto best_question = Question();  //keep track of the feature / value that produced it
+    int num_cols = meta.labels.size();
+    // TODO: find the best split among all features and feature values
+    for (int col = 0; col < num_cols - 1; col++) {
+
+    }
 
 
 
-  return forward_as_tuple(best_gain, best_question);
+
+    return forward_as_tuple(best_gain, best_question);
 }
 
 const double Calculations::gini(const ClassCounter& counts, double N) {  
   // compute gini index, given class counts and the dataset size;
   // Gini(S) = 1 - sum(pi^2)
-  double impurity = 1.0;
-  for (ClassCounter::const_iterator cit = counts.begin(); cit != counts.end(); cit++) {
-      impurity -= (cit->second / N) * (cit->second / N);
-  }
-  return impurity;
+    double impurity = 1.0;
+    for (ClassCounter::const_iterator cit = counts.begin(); cit != counts.end(); cit++) {
+        impurity -= (cit->second / N) * (cit->second / N);
+    }
+    return impurity;
 }
 
+/*ind the best split value for a discrete ordinal feature*/
 tuple<std::string, double> Calculations::determine_best_threshold_numeric(Data& data, int col) {
-  //TODO: find the best split value for a discrete ordinal feature
-  sort_col(data, col);
+    sort_col(data, col);
     double best_loss = std::numeric_limits<float>::infinity();
     double current_loss = 0.0;
     std::string best_thresh;
@@ -66,19 +67,20 @@ tuple<std::string, double> Calculations::determine_best_threshold_numeric(Data& 
     ClassCounts class_counts = classCounts(data, col);
     ClassCounter results = class_counts.ctr;  // 最后decision的统计
     FeatureDecisionCounter results_of_feature = class_counts.ctr_of_feature_decision;  // 一个feature里的decision的统计
-    ClassCounter s1; // s1此时为累计值
-    ClassCounter s2; // residual values
+    ClassCounter s1; // accumulated values
+    ClassCounter s2 = results; // residual values
+    ClassCounter temp;
     double gini_s = gini(results, data_size);  // gini of results
     double gini_s1;
     double gini_s2;
     double gain_gini;
     double num_decisions_per_value = 0.0;
 
-    for (FeatureDecisionCounter::iterator fit = results_of_feature.begin(); fit != results_of_feature.end(); fit++) {  // 迭代feature里所有可能的value
-        s1 = fit->second; // 每个value对应的decisions 
-        s2 = results;
-        for (ClassCounter::iterator cit = s1.begin(); cit != s1.end(); cit++) {  // 遍历每个value对应的decisions
+    for (FeatureDecisionCounter::iterator fit = results_of_feature.begin(); fit != results_of_feature.end(); fit++) {  // 迭代feature里所有可能的value       
+        temp = fit->second; // 每个value对应的decisions 
+        for (ClassCounter::iterator cit = temp.begin(); cit != temp.end(); cit++) {  // 遍历每个value对应的decisions
             num_decisions_per_value += double(cit->second);  // 将decision数加起来
+            s1[cit->first] += cit->second;
             s2.at(cit->first) -= cit->second;
         }
         gini_s1 = gini(s1, num_decisions_per_value);
@@ -101,43 +103,43 @@ tuple<std::string, double> Calculations::determine_best_threshold_numeric(Data& 
     find the best split value for a categorical feature
 */
 tuple<std::string, double> Calculations::determine_best_threshold_cat(Data& data, int col) {
-  sort_col(data, col);
-  double best_loss = std::numeric_limits<float>::infinity();
-  double current_loss = 0.0;
-  std::string best_thresh;
-  double data_size = data.size();
-  ClassCounts class_counts = classCounts(data, col);
-  ClassCounter results = class_counts.ctr;  // 最后decision的统计
-  FeatureDecisionCounter results_of_feature = class_counts.ctr_of_feature_decision;  // 一个feature里的decision的统计
-  ClassCounter s1;
-  ClassCounter s2; // residual values
-  double gini_s = gini(results, data_size);  // gini of results
-  double gini_s1;
-  double gini_s2;
-  double gain_gini;
-  double num_decisions_per_value = 0.0;
+    sort_col(data, col);
+    double best_loss = std::numeric_limits<float>::infinity();
+    double current_loss = 0.0;
+    std::string best_thresh;
+    double data_size = data.size();
+    ClassCounts class_counts = classCounts(data, col);
+    ClassCounter results = class_counts.ctr;  // 最后decision的统计
+    FeatureDecisionCounter results_of_feature = class_counts.ctr_of_feature_decision;  // 一个feature里的decision的统计
+    ClassCounter s1;
+    ClassCounter s2; // residual values
+    double gini_s = gini(results, data_size);  // gini of results
+    double gini_s1;
+    double gini_s2;
+    double gain_gini;
+    double num_decisions_per_value = 0.0;
   
-  for (FeatureDecisionCounter::iterator fit = results_of_feature.begin(); fit != results_of_feature.end(); fit++) {  // 迭代feature里所有可能的value
-      s1 = fit->second; // 每个value对应的decisions 
-      s2 = results;
-      for (ClassCounter::iterator cit = s1.begin(); cit != s1.end(); cit++) {  // 遍历每个value对应的decisions
-          num_decisions_per_value += double(cit->second);  // 将decision数加起来
-          s2.at(cit->first) -= cit->second;
-      }
-      gini_s1 = gini(s1, num_decisions_per_value);
-      gini_s2 = gini(s2, data_size - num_decisions_per_value);
-      current_loss = (num_decisions_per_value / data_size) * gini_s1 + ((data_size - num_decisions_per_value) / data_size) * gini_s2;
-      num_decisions_per_value = 0.0;
-      if (current_loss < best_loss) {
-          best_loss = current_loss;
-          best_thresh = fit->first;
-          if (best_loss == 0.0) {
-              break;
-          }
-      }
-  }
-  gain_gini = gini_s - best_loss;
-  return forward_as_tuple(best_thresh, gain_gini);
+    for (FeatureDecisionCounter::iterator fit = results_of_feature.begin(); fit != results_of_feature.end(); fit++) {  // 迭代feature里所有可能的value
+        s1 = fit->second; // 每个value对应的decisions 
+        s2 = results;
+        for (ClassCounter::iterator cit = s1.begin(); cit != s1.end(); cit++) {  // 遍历每个value对应的decisions
+            num_decisions_per_value += double(cit->second);  // 将decision数加起来
+            s2.at(cit->first) -= cit->second;
+        }
+        gini_s1 = gini(s1, num_decisions_per_value);
+        gini_s2 = gini(s2, data_size - num_decisions_per_value);
+        current_loss = (num_decisions_per_value / data_size) * gini_s1 + ((data_size - num_decisions_per_value) / data_size) * gini_s2;
+        num_decisions_per_value = 0.0;
+        if (current_loss < best_loss) {
+            best_loss = current_loss;
+            best_thresh = fit->first;
+            if (best_loss == 0.0) {
+                break;
+            }
+        }
+    }
+    gain_gini = gini_s - best_loss;
+    return forward_as_tuple(best_thresh, gain_gini);
 }
 
 Calculations::ClassCounts classCounts(const Data& data, int col) {  // 输入data
@@ -166,7 +168,7 @@ Calculations::ClassCounts classCounts(const Data& data, int col) {  // 输入dat
 }
 
 bool Calculations::compare_rows(const VecS& v1, const VecS& v2, int col) {
-    return v1[col] < v2[col];
+    return v1[col] > v2[col];
 }
 
 void Calculations::sort_col(Data& data, int col) {
